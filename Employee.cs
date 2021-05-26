f1a11b4fdda06f1359581a8f9475105e390a3a3f
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-05-26 17:01:48 +0530

    [Harsh]Refactor: User case 2 to calculate part,full time wage and

--- Employee.cs
+++ Employee.cs
@@ -9,30 +9,39 @@ namespace EmployeeWage
         readonly int IS_FULL_TIME = 1;
         readonly int EMP_RATE_PER_HOUR = 20;
         readonly int FULL_DAY_HOUR = 8;
+        readonly int IS_PART_TIME = 2;
+        int Emphrs = 0;
 
         public void Attendance()
         {
             Random random = new Random();
-            int Attendance = random.Next(0, 2);
+            int Attendance = random.Next(0, 3);
             if (Attendance == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is present and full time.");
+                Emphrs = 8;
+                CalculateWage();
+            }
+            else if (Attendance == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is part time");
+                Emphrs = 4;
                 CalculateWage();
             }
             else
             {
                 Console.WriteLine("Employee is absent");
-                Console.WriteLine("Employee wage is null");
-
+                Console.WriteLine("Employee Wage is Null");
             }
         }
 
+
         public void CalculateWage()
         {
-            int Wage = FULL_DAY_HOUR * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Wage: " + Wage);
-            Console.ReadLine();
-        }
+                int Wage = Emphrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Wage: " + Wage);
+                Console.ReadLine();
+            }
 
-    }
+        }        
 }
