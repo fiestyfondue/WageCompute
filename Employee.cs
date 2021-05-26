ad394b448b701851b3a856b69e958eff824dca55
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-05-26 18:28:13 +0530

    [Harsh]Refactor: Employee wage using switch

--- Employee.cs
+++ Employee.cs
@@ -6,42 +6,33 @@ namespace EmployeeWage
 {
     class Employee
     {
-        readonly int IS_FULL_TIME = 1;
-        readonly int EMP_RATE_PER_HOUR = 20;
-        readonly int FULL_DAY_HOUR = 8;
-        readonly int IS_PART_TIME = 2;
+        public const int  IS_FULL_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int FULL_DAY_HOUR = 8;
+        public const int IS_PART_TIME = 2;
         int Emphrs = 0;
+        int Wage = 0;
+
 
         public void Attendance()
         {
             Random random = new Random();
             int Attendance = random.Next(0, 3);
-            if (Attendance == IS_FULL_TIME)
+            switch (Attendance)
             {
-                Console.WriteLine("Employee is present and full time.");
-                Emphrs = 8;
-                CalculateWage();
+                case IS_PART_TIME:
+                    Emphrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    Emphrs = 8;
+                    break;
+                default:
+                    Emphrs = 0;
+                    break;
             }
-            else if (Attendance == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is part time");
-                Emphrs = 4;
-                CalculateWage();
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                Console.WriteLine("Employee Wage is Null");
-            }
+            Wage = Emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Wage: " + Wage);
+            Console.ReadLine();
         }
-
-
-        public void CalculateWage()
-        {
-                int Wage = Emphrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Wage: " + Wage);
-                Console.ReadLine();
-            }
-
-        }        
+    }
 }
